56f3fa5652e2d607b53ba07cacbc25b794fc6f6c
Damaris-PC <damisoliveira@gmail.com>
2020-09-04 15:55:33 -0300

    Trabalho crud basico

---
+++ CadastroData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CadastroData.MvcApplication" Language="C#" %>
---
+++ CadastroData/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CadastroData.Models
+{
+    public class Pessoa
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+    }
+}
